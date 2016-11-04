784d4d80a25bf2d2a80ba75fa3563eed07999789
Ray Cohen <rcohen@paperlesspost.com>
2016-11-04 15:40:22 -0400

    add an application template and a component

---
+++ app/components/demo-main.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  showingVersion: false,
+  version: function() {
+    let metaTag = document.querySelector('meta[name=version]');
+    if (!metaTag) return 'unknown';
+
+    return metaTag.getAttribute('content');
+  },
+  actions: {
+    showVersion() {
+      this.set('showingVersion', true);
+    }
+  }
+});
