5f8cefa784bddec5bdcae31985354237d71c8092
Ray Cohen <rcohen@paperlesspost.com>
2016-11-07 10:22:46 -0500

    been using react for too long

--- app/components/demo-main.js
+++ app/components/demo-main.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showingVersion: false,
-  demoData: function() {
+  demoData: Ember.computed(function() {
     let metaTag = document.querySelector('meta[name=demo-data]');
     if (!metaTag) {
       return {version: 'unknown'};
     }
 
     return JSON.parse(metaTag.getAttribute('content'));
-  },
+  }),
   actions: {
     showVersion() {
       this.set('showingVersion', true);
